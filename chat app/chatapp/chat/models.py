a07231b6388ce67105b0756646bf858f4646ea50
samanemkul <nemkulsama1@gmail.com>
2024-08-27 18:23:08 +0545

    added

---
+++ chat app/chatapp/chat/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Room(models.Model):
+    room_name = models.CharField(max_length=50, default=0)
+    def __str__(self):
+        return self.room_name
+
+class Message(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, default=0)
+    sender = models.CharField(max_length=50,  default=0)
+    message = models.TextField()
+
+    def __str__(self):
+        return f"{str(self.room)} - {self.sender}"
