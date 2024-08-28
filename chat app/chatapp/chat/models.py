1d8db098785206f34db50dda35218c58cb2e11ff
samanemkul <nemkulsama1@gmail.com>
2024-08-28 14:19:34 +0545

    completed

--- chat app/chatapp/chat/models.py
+++ chat app/chatapp/chat/models.py
@@ -9,6 +9,7 @@ class Message(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE, default=0)
     sender = models.CharField(max_length=50,  default=0)
     message = models.TextField()
+    time_stamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{str(self.room)} - {self.sender}"
--- chat app/chatapp/chatapp/settings.py
+++ chat app/chatapp/chatapp/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # 'daphne',
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'chat',
-    # 'channels',
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -71,6 +71,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'chatapp.wsgi.application'
+ASGI_APPLICATION = 'chatapp.asgi.application'
 
 
 # Database
@@ -124,3 +125,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CHANNEL_LAYERS = {"default":{"BACKEND" :"channels.layers.InMemoryChannelLayer"}}
